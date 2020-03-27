a58221d2ace201a34137244697367ab8c92eecaa
Juan Manuel Grimal <juanmag4@MacBook-Pro-de-Juan.local>
2020-03-26 21:50:38 -0300

    calculate post date

--- src/Components/Post.tsx
+++ src/Components/Post.tsx
@@ -1,27 +1,50 @@
 import React from 'react';
+import moment, { Moment } from 'moment';
+
+const calculateTime = (minutes: number) => {
+  let timeAgo: string = '';
+  if (minutes < 60) {
+    timeAgo = `${Math.trunc(minutes)} Minutes`;
+  } else if (minutes > 60 && minutes < 1440) {
+    timeAgo = `${Math.trunc(minutes / 60)} Hours`;
+  } else {
+    timeAgo = `${Math.trunc(minutes / 60 / 24)} Days`;
+  }
+
+  return `${timeAgo} ago`;
+};
 
 const Post = (props: any) => {
+  const { title, author, url, thumbnail, num_comments, created_utc } = props.post.data;
+
   const openImage = () => {
     if (url) {
       window.open(url);
     }
   }
+
+  const getTimeAgo = () => {
+    const postDate: Moment = moment.utc(created_utc * 1000);
+    const currentDate: Moment = moment();
+    const timeAgo: number = currentDate.diff(postDate, 'minute');
+
+    return calculateTime(timeAgo);
+  }
   
-  const { title, author, url, thumbnail, num_comments } = props.post.data;
   return (
-    <div className="ui card">
-      <div className="content">
-        <div className="right floated meta">14h</div>
+    <div className='ui card'>
+      <div className='content'>
+        <div className='right floated meta'>{getTimeAgo()}</div>
         {author}
       </div>
-      <div className="content">
+      <div className='content'>
         {title}
       </div>
-      {thumbnail ? <div onClick={openImage} className="image">
+      {thumbnail ? <div onClick={openImage} className='image'>
         <img src={thumbnail} />
       </div> : <div></div>}
-      <div className="content">
-        <i className="comment icon"></i>
+      <div className='content'>
+        <i className='comment icon'></i>
         {num_comments} comments
       </div>
     </div>
